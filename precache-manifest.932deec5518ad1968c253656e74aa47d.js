335f7c44e771a5b8081093cf26dfc21fbe3e6e9d
fasez26 <nallely232@hotmail.com>
2020-07-18 22:49:28 -0500

    Updates

---
+++ precache-manifest.932deec5518ad1968c253656e74aa47d.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8d6c0d9dff205c88463bce8de4652f",
+    "url": "/CDMX009-BurgerQueen/index.html"
+  },
+  {
+    "revision": "591407d7c097c76110ed",
+    "url": "/CDMX009-BurgerQueen/static/css/main.855d5e3f.chunk.css"
+  },
+  {
+    "revision": "296dcc4e47c25a638078",
+    "url": "/CDMX009-BurgerQueen/static/js/2.51cc952a.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/CDMX009-BurgerQueen/static/js/2.51cc952a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "591407d7c097c76110ed",
+    "url": "/CDMX009-BurgerQueen/static/js/main.c499af3c.chunk.js"
+  },
+  {
+    "revision": "7c749190c2c9b4748a87",
+    "url": "/CDMX009-BurgerQueen/static/js/runtime-main.467ef1bc.js"
+  },
+  {
+    "revision": "19e2db38d6af198fad67893613d1a1d1",
+    "url": "/CDMX009-BurgerQueen/static/media/back.19e2db38.svg"
+  },
+  {
+    "revision": "63d03a427cfefe3b9459fb39f57c72af",
+    "url": "/CDMX009-BurgerQueen/static/media/bill.63d03a42.svg"
+  },
+  {
+    "revision": "4845624710bd670e4f4e1be42a523c4c",
+    "url": "/CDMX009-BurgerQueen/static/media/breakfast.48456247.svg"
+  },
+  {
+    "revision": "088ec8e79a18be2f907a9412691d4b7f",
+    "url": "/CDMX009-BurgerQueen/static/media/burger.088ec8e7.svg"
+  },
+  {
+    "revision": "a70ae04dcf67d4fba46a937c63149631",
+    "url": "/CDMX009-BurgerQueen/static/media/chef.a70ae04d.svg"
+  },
+  {
+    "revision": "a0b9357e1656db852aa5d1ef1c1997c3",
+    "url": "/CDMX009-BurgerQueen/static/media/clock1.a0b9357e.svg"
+  },
+  {
+    "revision": "aab7645b4aba3e98a30f49542d83918a",
+    "url": "/CDMX009-BurgerQueen/static/media/hourglass.aab7645b.svg"
+  },
+  {
+    "revision": "6493ff32f13eccf863db9ed77980fa97",
+    "url": "/CDMX009-BurgerQueen/static/media/house.6493ff32.svg"
+  },
+  {
+    "revision": "cad6a5faa8f35c43563510797a6e6062",
+    "url": "/CDMX009-BurgerQueen/static/media/logout.cad6a5fa.svg"
+  },
+  {
+    "revision": "f5979b7e8d36f6563f6ba25465d9d95c",
+    "url": "/CDMX009-BurgerQueen/static/media/meal.f5979b7e.svg"
+  },
+  {
+    "revision": "d0422f8806647a6cad2af668c2f7abaf",
+    "url": "/CDMX009-BurgerQueen/static/media/orderReady.d0422f88.svg"
+  },
+  {
+    "revision": "10a132348c69cc8f2a565c0c6d06c233",
+    "url": "/CDMX009-BurgerQueen/static/media/padlock.10a13234.svg"
+  },
+  {
+    "revision": "dd6d5874b4acaa400b63f292ebd1b038",
+    "url": "/CDMX009-BurgerQueen/static/media/sign.dd6d5874.svg"
+  },
+  {
+    "revision": "f3fb1415fa05e46e12721dc4d5a4b564",
+    "url": "/CDMX009-BurgerQueen/static/media/table.f3fb1415.svg"
+  },
+  {
+    "revision": "b8ce73430e168c1f9ae7cbc0019d989e",
+    "url": "/CDMX009-BurgerQueen/static/media/title.b8ce7343.svg"
+  },
+  {
+    "revision": "cb0c1f72336ef0d47fcf995a1fc29980",
+    "url": "/CDMX009-BurgerQueen/static/media/user.cb0c1f72.svg"
+  },
+  {
+    "revision": "29bb8a2c669ac3efb710ed946f633820",
+    "url": "/CDMX009-BurgerQueen/static/media/waiter.29bb8a2c.svg"
+  },
+  {
+    "revision": "36334993a301ee14abfcac4cf57f133e",
+    "url": "/CDMX009-BurgerQueen/static/media/whiterectangle.36334993.svg"
+  },
+  {
+    "revision": "f8418dbc79c86dcb49dc539b0d180676",
+    "url": "/CDMX009-BurgerQueen/static/media/yellowrec.f8418dbc.svg"
+  }
+]);
